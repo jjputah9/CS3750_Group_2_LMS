0a97e021b40d14790cf2ec055b1fa1233dc7301f
jjputah9 <77799034+jjputah9@users.noreply.github.com>
2026-01-21 09:58:11 -0700

    Initial Commit

---
+++ LMS/LMS/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
