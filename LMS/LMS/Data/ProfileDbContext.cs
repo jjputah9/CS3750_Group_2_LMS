4280a4ea3148ea3974a528a3ecd048346d696be0
jayanti07 <jayantiacharya@mail.weber.edu>
2026-01-23 13:51:01 -0700

    Add Profile feature: - UserProfile model with validation - ProfileDbContext for profile data - ProfileService for CRUD operations - Profile page with form - Navbar integration

---
+++ LMS/LMS/Data/ProfileDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LMS.Models;
+
+namespace LMS.Data
+{
+    public class ProfileDbContext : DbContext
+    {
+        public ProfileDbContext(DbContextOptions<ProfileDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
