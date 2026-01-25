76f0083b43ec3b29b53d19eb9e4d9b0e74feb999
KianThyberg <kianthyberg@mail.weber.edu>
2026-01-25 14:50:37 -0700

    Course Class Scaffold

--- LMS/LMS/Data/ApplicationDbContext.cs
+++ LMS/LMS/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using LMS.Models;
 
 namespace LMS.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<LMS.Models.Course> Course { get; set; } = default!;
     }
 }
---
+++ LMS/LMS/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using LMS.Data;
+using LMS.Models;
+
+namespace LMS.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly LMS.Data.ApplicationDbContext _context;
+
+        public CreateModel(LMS.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Course.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
