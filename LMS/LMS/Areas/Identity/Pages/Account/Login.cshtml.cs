31cce81d89c47263764344d55285ffc9e262512d
Hayden Swain-Schmit <haydenswainschmit@mail.weber.edu>
2026-01-26 20:36:10 -0700

    Same as previous commit, missed some lines

--- LMS/LMS/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ LMS/LMS/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -85,8 +85,14 @@ namespace LMS.Areas.Identity.Pages.Account
             public bool RememberMe { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
         {
+            // redirect logged in users
+            if (User.Identity.IsAuthenticated)
+            { 
+                return LocalRedirect("/Dashboard");
+            }
+
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
@@ -100,6 +106,8 @@ namespace LMS.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             ReturnUrl = returnUrl;
+
+            return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
