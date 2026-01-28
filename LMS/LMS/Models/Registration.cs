0025df4f18843aa40b5f9407727a9a2d95ce5278
KianThyberg <kianthyberg@mail.weber.edu>
2026-01-28 11:17:09 -0700

    Registration Model Class
    Added Model class, needs scaffolding

---
+++ LMS/LMS/Models/Registration.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using LMS.models;
+
+
+namespace LMS.Models;
+
+//Registration Association class handles relationship between student and courses
+public class Registration
+{
+    public int Id {get; set; } //Primary Key
+
+    [Required]
+    public string? StudentID {get; set;} //foreign key for student table
+
+    [Required]
+    public string? CourseID {get; set; } //foreign key for course table
+
+    [DataType(DataType.DateTime)]
+    public DateTime RegistrationDateTime {get; set;} //date and time of registration
+}
