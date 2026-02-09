76f8b146e36324442dd50d592907629faa6703c6
unknown <komlanassogba@mail.weber.edu>
2026-02-09 08:37:48 -0700

    Adding the Chart Page

---
+++ LMS/LMS/Pages/Charts/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Pages.Charts
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
