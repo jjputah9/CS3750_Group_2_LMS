48b3f4cf97719c8bbe07b9a5b93111e1ea46e782
Hayden Swain-Schmit <haydenswainschmit@mail.weber.edu>
2026-02-23 10:28:50 -0700

    start payment tests

---
+++ LMS/LMS_Test/PaymentTests.cs
@@ -0,0 +1,16 @@
+using LMS.Data;
+using LMS.Models;
+using LMS.Pages.Registrations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS_Test;
+
+[TestClass]
+public class PaymentTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
